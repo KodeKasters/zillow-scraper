dc303ef6ddd24e08ef61d55190b9a753a3f7b694
Alejandro S <alexsanchez0512@yahoo.com>
2024-04-04 20:43:32 -0400

    Scrapes ids

--- scrape.js
+++ scrape.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const scrape = async () => {
   const browser = await puppeteer.launch({
+    headless: false,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -20,7 +21,15 @@ const scrape = async () => {
   try {
     const page = await browser.newPage();
     const url = buildUrlFromQueryParams(zillow);
-    //await page.goto(url);
+    await page.goto(url);
+
+    const ids = await page.evaluate(() => {
+      const divs = document.querySelectorAll('article.property-card');
+      return Array.from(divs).map(div => div.getAttribute("id"))
+    })
+
+    console.log("ids:", ids)
+
     console.log(url);
   } catch (err) {
     console.error(err);
